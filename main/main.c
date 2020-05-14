70a5d43f0c218c0f7f3f2d62f40bc1e8cc78be14
Tal Rasha <524948250@qq.com>
2020-05-14 11:31:25 +0800

    [issue 5] deploying other repo in travis (#6)
    * do clone
    
    * do make
    
    * use functions of matrix lib
    
    * set environment var
    
    * use root permission to do copy
    
    * minor fix
    
    * do change directory after installing dependency
    
    Co-authored-by: Horardrim-DeckardCain <482612641@qq.com>

--- main/main.c
+++ main/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <event2/util.h>
 #include <event2/event.h>
 #include <event2/thread.h>
+
+#include <matrix_c/hm_char_matrix.h>
 
 int main(int argc, char ** argv)
 {
@@ -11,5 +14,10 @@ int main(int argc, char ** argv)
 
     event_base_free(base);
     base = 0;
+
+    hm_char_matrix_t * test_matrix = _hm_char_matrix_t(10, 10);
+    test_matrix->set_matrix_value(test_matrix, '%', 5, 5);
+    test_matrix->display_matrix(test_matrix);
+    hm_char_matrix_t_(test_matrix);
     return 0;
 }
